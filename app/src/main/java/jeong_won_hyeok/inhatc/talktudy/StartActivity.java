a0003f5fac0ba1b4dc88aca065e545af3ed871c8
KHG <fdsq562>
2020-06-20 03:19:12 +0900

    시작화면_수정중

---
+++ app/src/main/java/jeong_won_hyeok/inhatc/talktudy/StartActivity.java
@@ -0,0 +1,29 @@
+package jeong_won_hyeok.inhatc.talktudy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        loadingHandler();
+    }
+
+    private void loadingHandler() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplication(), MapsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
